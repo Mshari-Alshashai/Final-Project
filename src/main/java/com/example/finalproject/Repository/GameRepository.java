e3b60cf57c54b34ad1a1e6442088a57da69ca2fd
Raghad <raghad444979@gmail.com>
2025-01-01 23:09:44 +0300

    Models , ODTO and repository

---
+++ src/main/java/com/example/finalproject/Repository/GameRepository.java
@@ -0,0 +1,9 @@
+package com.example.finalproject.Repository;
+
+import com.example.finalproject.Model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game,Integer> {
+}
